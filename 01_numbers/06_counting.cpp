608bd047110f3c6b2e7723166f541e2934a862fd
Madhur Yadav <madhuryadav1444@gmail.com>
2024-02-22 12:24:48 +0530

    count

--- 01_numbers/06_counting.cpp
+++ 01_numbers/06_counting.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-//Brute Force
+//Brute Force (Counting Pair)
 // int countPair(int arr[], int size, int k){
 //     int count = 0;
 //     for(int i=0; i<size; i++){
@@ -12,7 +12,7 @@ using namespace std;
 //     return count;    
 // }
 
-// Optimized - Using the property of Modulus
+// Optimized - Using the property of Modulus (Counting Pair)
 int countPair(int arr[], int size, int k) {
     // Step 1: Initialize array elements to zero
     int count[k];
@@ -46,6 +46,66 @@ int countPair(int arr[], int size, int k) {
     return ans;
 }
 
+
+// Brute Force (Triplet Count)
+// int countTriplet(int arr[], int size, int k){
+//     int cnt = 0;
+
+//     for(int i=0; i<size; i++){
+//         for(int j=i+1; j<size; j++){
+//             for(int m=j+1; m<size; m++){
+//                 if((arr[i] + arr[j] + arr[m])%k == 0) cnt++;
+//             }
+//         }
+//     }
+// }
+
+
+// Optimized -- Using the property of modulus (Triplet Count)
+int countTriplet(int arr[], int size, int k){
+    
+    int count[k];
+    for (int i = 0; i < k; i++) {
+        count[i] = 0;
+    }
+
+    for (int i = 0; i < size; i++) {
+        count[arr[i] % k]++;
+    }
+
+    int ans = 0;
+
+    // Iterate over all possible pairs of remainders (i, j)
+    for (int i = 0; i < k; i++) {
+        for (int j = i; j < k; j++) {
+            int thirdRem = (k - (i + j) % k) % k;
+
+            if (thirdRem < j) {
+                continue;
+            }
+
+            if (i == j && j == thirdRem) {
+                // Count triplets with all three remainders being the same
+                ans += (count[i] * (count[i] - 1) * (count[i] - 2)) / 6;
+            } else if (i == j) {
+                // Count triplets with two remainders being the same
+                ans += (count[i] * (count[i] - 1) / 2) * count[thirdRem];
+            } else if (i == thirdRem) {
+                // Count triplets with two remainders being the same
+                ans += (count[i] * (count[i] - 1) / 2) * count[j];
+            } else if (j == thirdRem) {
+                // Count triplets with two remainders being the same
+                ans += (count[j] * (count[j] - 1) / 2) * count[i];
+            } else {
+                // Count triplets with all three remainders being different
+                ans += count[i] * count[j] * count[thirdRem];
+            }
+        }
+    }
+
+    return ans;    
+}
+
 int main() {
     int size, k;
     cin>>size>>k;
@@ -55,7 +115,8 @@ int main() {
         cin>>arr[i];
     }
     
-    cout<<countPair(arr, size, k);
+    cout<<countPair(arr, size, k)<<endl;
+    cout<<countTriplet(arr, size, k)<<endl;
 
     return 0;
 }
