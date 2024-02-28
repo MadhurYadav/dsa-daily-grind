3c3b78c13bd824c25c7d3d91a9a276d2011d5f08
Madhur Yadav <madhuryadav1444@gmail.com>
2024-02-28 09:12:31 +0530

    arr

--- 03_arrays/striver/09_missingNum.cpp
+++ 03_arrays/striver/09_missingNum.cpp
@@ -137,14 +137,16 @@ Function findMissingNumberXOR(arr: array of integers, N: integer) -> integer:
 
 */
 int missingNumberXOR(vector<int> &v, int N){
-    int xor1 = 0, xor2 = 0;
+        int xor1 = 0, xor2 = 0;
+        
 
-    for(int i=0; i<N-1; i++){
-        xor2 = xor2 ^ v[i]; // XOR of array elements
-        xor1 = xor1 ^ (i+1); //XOR up to [1...N-1]
-    }
-    xor1 = xor1 ^ N; //XOR up to [1...N]
-    return (xor1 ^ xor2); // the missing number
+        for(int i=0; i<N-1; i++){
+            xor2 = xor2 ^ v[i]; // XOR of array elements
+            xor1 = xor1 ^ (i+1); //XOR up to [1...N-1]
+        }
+        xor1 = xor1 ^ N; //XOR up to [1...N]
+        return (xor1 ^ xor2); // the missing number
+
 }
 
 int main() {
